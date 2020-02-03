df7da84609d43f446b515415bfa1510a7d783ff1
zhuyie <zhuyie@gmail.com>
2020-02-03 20:42:48 +0800

    Add fesetenv

--- floating-point/denormalized.c
+++ floating-point/denormalized.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <fenv.h>
 
 int test1()
 {
@@ -35,6 +36,11 @@ int main(int argc, char* argv[])
 {
 	if (argc >= 2 && strcmp(argv[1], "2") == 0)
 	{
+		test2();
+	}
+	else if (argc >= 2 && strcmp(argv[1], "3") == 0)
+	{
+		fesetenv(FE_DFL_DISABLE_SSE_DENORMS_ENV);
 		test2();
 	}
 	else
