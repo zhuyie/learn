2391ac9e47a82b4dc553feaac24eb36dd26bd923
zhuyie <zhuyie@gmail.com>
2020-02-05 19:38:47 +0800

    Add function test4

--- floating-point/test/operations.c
+++ floating-point/test/operations.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits.h> // for INT_MAX
+#include <float.h>  // for FLT_MAX
 
 void test1()
 {
@@ -53,10 +55,43 @@ void test3()
 	printf("\n");
 }
 
+void test4()
+{
+	printf("test4:\n");
+	printf("------------------------------\n");
+
+	float f0 = 567.8f;
+	printf("float(%f) => int(%d)\n", f0, (int)f0);
+	
+	for (int i = 0; i < 8; i++)
+	{
+		int n = 16777213 + i;
+		printf("int(%d) => float(%f)\n", n, (float)n);
+	}
+
+	int i1 = 1000000000;
+	printf("int(%d) => float(%f)\n", i1, (float)i1);
+	int i2 = 1000000101;
+	printf("int(%d) => float(%f)\n", i2, (float)i2);
+
+	int i3 = INT_MAX;
+	printf("int(%d) => float(%f)\n", i3, (float)i3);
+	float f4 = INT_MAX;  f4 += 1.0f;
+	printf("float(%f) => int(%d 0x%08x)\n", f4, (int)f4, (int)f4);
+	long long i5 = (long long)INT_MAX + 1;
+	printf("long long(%lld) => int(%d)\n", i5, (int)i5);
+	
+	float f6 = FLT_MAX;
+	printf("float(%f) => int(%d)\n", f6, (int)f6);
+
+	printf("\n");
+}
+
 int main()
 {
 	test1();
 	test2();
 	test3();
+	test4();
 	return 0;
 }
